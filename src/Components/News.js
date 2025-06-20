44f684cd8c8a2ddb373e7c7cbbcbce131b1ce685
deephardsaini <deep.98saini@gmail.com>
2025-06-20 12:20:00 +0530

    Fix imageUrl prop issue in Newsitem

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,169 +1,126 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Newsitem from "./Newsitem";
 import spinner from "../spinner.gif";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "us",
-    pageSize: 15,
-    category: "general",
-  };
+const News = ({ country, pageSize, category, setProgress }) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-      hasMore: true,
-    };
-  }
-
-  async componentDidMount() {
-    this.fetchNews();
-  }
-
- fetchNews = async () => {
-  this.setState({ loading: true });
-  const { page } = this.state;
-  const { pageSize, country, category } = this.props;
-  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
-
-  const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
-  
-  const response = await fetch(url);
-
-  // ⛔ Check for 429 error
-  if (response.status === 401) {
-    alert("Rate limit exceeded. Please try again later.");
-    this.setState({ loading: false, hasMore: false });
-    return;
-  }
-
-  const parseData = await response.json();
-
-  this.setState({
-    articles: parseData.articles || [],
-    totalResults: parseData.totalResults || 0,
-    loading: false,
-    hasMore: (parseData.articles?.length || 0) >= pageSize,
-  });
-};
-
-
- fetchMoreData = async () => {
-  const nextPage = this.state.page + 1;
-  const { pageSize, country, category } = this.props;
-  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
-
-  this.props.setProgress?.(10);
-
-  const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${nextPage}&pageSize=${pageSize}`;
-  const response = await fetch(url);
-
-  // ⛔ Check for 429 error
-  if (response.status === 401) {
-    alert("Rate limit exceeded. Please try again later.");
-    this.setState({ hasMore: false });
-    this.props.setProgress?.(100);
-    return;
-  }
-
-  this.props.setProgress?.(30);
-  const parseData = await response.json();
-  this.props.setProgress?.(70);
-
-  this.setState((prevState) => ({
-    page: nextPage,
-    articles: prevState.articles.concat(parseData.articles || []),
-    totalResults: parseData.totalResults || 0,
-    hasMore:
-      prevState.articles.length + (parseData.articles?.length || 0) <
-      (parseData.totalResults || 0),
-  }));
-
-  this.props.setProgress?.(100);
-};
-
-
-
-  capitalizeFirstLetter(string) {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
 
-  render() {
-    return (
-      <div className="container my-3">
-        <h1 className="text-center">
-          News Monkey - <span>Top</span>{" "}
-          <span style={{ marginLeft: "5px" }}>
-            {this.capitalizeFirstLetter(this.props.category)}
-          </span>{" "}
-          Headlines
-        </h1>
+  const fetchNews = async () => {
+    setLoading(true);
+    const apiKey = process.env.REACT_APP_NEWS_API_KEY;
 
-        {/* Show spinner before initial load */}
-        {this.state.loading && this.state.articles.length === 0 && (
-          <div className="text-center my-3">
-            <img src={spinner} alt="loading" style={{ width: "40px" }} />
-          </div>
-        )}
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
 
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.hasMore}
-          loader={
-            this.state.hasMore && (
-              <div className="text-center my-3">
-                <img src={spinner} alt="loading" style={{ width: "40px" }} />
-              </div>
-            )
-          }
-          endMessage={
-            <p className="text-center mt-4">
-              <b>Yay! You have seen all the news.</b>
-            </p>
-          }
-        >
-          <div className="row mt-4 gx-0">
-            {this.state.articles.map((element, index) => (
-              <div className="col-md-4" key={index}>
-                <Newsitem
-                  title={
-                    element.title ? element.title.slice(0, 45) : "No Title"
-                  }
-                  description={
-                    element.description
-                      ? element.description.slice(0, 88)
-                      : "No Description Available"
-                  }
-                  imageurl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://www.dianomi.com/img/a/sav2/349880/4/300x160.jpg"
-                  }
-                  url={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            ))}
-          </div>
-        </InfiniteScroll>
-      </div>
+    const response = await fetch(url);
+
+    if (response.status === 401 || response.status === 429) {
+      alert("Rate limit exceeded. Please try again later.");
+      setLoading(false);
+      setHasMore(false);
+      return;
+    }
+
+    const parseData = await response.json();
+    setArticles(parseData.articles || []);
+    setTotalResults(parseData.totalResults || 0);
+    setLoading(false);
+    setHasMore((parseData.articles?.length || 0) >= pageSize);
+  };
+
+  useEffect(() => {
+    fetchNews();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchMoreData = async () => {
+    const nextPage = page + 1;
+    const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+
+    if (setProgress) setProgress(10);
+
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${nextPage}&pageSize=${pageSize}`;
+    const response = await fetch(url);
+
+    if (response.status === 401 || response.status === 429) {
+      alert("Rate limit exceeded. Please try again later.");
+      setHasMore(false);
+      if (setProgress) setProgress(100);
+      return;
+    }
+
+    if (setProgress) setProgress(30);
+
+    const parseData = await response.json();
+
+    if (setProgress) setProgress(70);
+
+    setArticles((prevArticles) =>
+      prevArticles.concat(parseData.articles || [])
     );
-  }
-}
+    setPage(nextPage);
+    setTotalResults(parseData.totalResults || 0);
+
+    setHasMore(
+      articles.length + (parseData.articles?.length || 0) <
+        (parseData.totalResults || 0)
+    );
+
+    if (setProgress) setProgress(100);
+  };
+
+  return (
+    <div className="container my-3">
+      <h1 className="text-center">
+        NewsMonkey - Top {capitalizeFirstLetter(category)} Headlines
+      </h1>
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<img src={spinner} alt="Loading more..." style={{ display: "block", margin: "auto", width: "40px" }} />}
+      >
+        <div className="row mt-4 gx-0">
+          {articles.map((element, index) => (
+            <div className="col-md-4" key={index}>
+              <Newsitem
+                title={element.title}
+                description={element.description}
+                imageUrl={element.urlToImage}
+                url={element.url}
+                author={element.author}
+                date={element.publishedAt}
+                source={element.source.name}
+              />
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+News.defaultProps = {
+  country: "us",
+  pageSize: 15,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+  setProgress: PropTypes.func,
+};
 
 export default News;
